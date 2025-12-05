8c6c1964f0d70471a758198e1b40230ba1e253f6
iamkrinjal <krinjalmatekar3@gmail.com>
2025-12-05 14:51:03 +0530

    up

--- main.tf
+++ main.tf
@@ -1,20 +1,44 @@
 provider "aws" {
-    region = "us-east-1"
-  
+  region = "us-east-1"
 }
 
-resource "aws_instance" "Staticweb" {
-  ami = var.my_ami
-  instance_type = var.my_instance
-  key_name = var.my_key
-  vpc_security_group_ids = [aws_security_group.staticweb_sg.id]
-  user_data = file(var.user_data)
-  tags = {
-    Name= "staticWeb"
+resource "aws_key_pair" "tf-key" {
+  key_name   = "static-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "tf-sg" {
+  name        = "allow-http"
+  description = "Allow HTTP inbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.Staticweb.public_ip
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.tf-key.key_name
+  security_groups = [aws_security_group.tf-sg.name]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "static-web"
+  }
 }
 
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,3 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
- 
