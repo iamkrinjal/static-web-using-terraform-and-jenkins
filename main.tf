42377a3e951447c1b0b79bd0328786d96ad3017e
iamkrinjal <krinjalmatekar3@gmail.com>
2025-12-05 16:37:44 +0530

    up

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ resource "aws_security_group" "tf-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -36,34 +42,7 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "staticweb_sg" {
-  name = "staticweb-sg"
-  description = "security group for staticweb"
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "allow all traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "public_ip" {
   value = aws_instance.web.public_ip
